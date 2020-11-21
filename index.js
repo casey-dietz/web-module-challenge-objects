fec4c360a29589c7ef04c7a94accfe730364ff13
Casey Dietz <casey.dietz@ihsmarkit.com>
2020-11-21 00:41:13 -0700

    MVP complete

--- index.js
+++ index.js
@@ -95,7 +95,8 @@ Using the reviews array above do the following: (no function needed)
   1. Following the same format (name, rating, feedback), add a new fictitious review object to the reviews array
   2. log the whole array to the console, make sure the new review is inside of it   
 */
-
+reviews.push({name: "Casey", rating: 5, feedback: "These pretzals are making me thirsty"})
+console.log(reviews);
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
@@ -104,7 +105,8 @@ Reyna's feedback is missing! Use what you know to do the following: (no function
   2. log the reviews array to the console to check your work
 */
 
-
+reviews[7].feedback = "this place is chill with really cool people, great for getting work done on weekdays"
+console.log(reviews[7])
 
 
 
@@ -119,8 +121,8 @@ Use the getReviewByIndex function below to do the following:
 */
 
 
-function getReviewByIndex(/*Your code here*/) {
-  /*Your code here*/
+function getReviewByIndex(array, index) {
+  return (`${array[index].name} gave the restaurant a ${array[index].rating} star review, and their feedback was: ${array[index].feedback}`)
 }
 
 
@@ -138,8 +140,10 @@ Use the getLastReview function below to do the following:
 */
 
 
-function getLastReview(/*Your code here*/) {
-  /*Your code here*/
+function getLastReview(objArray) {
+  let array = objArray[objArray.length - 1]
+    console.log(`${array.name} gave the restaurant a ${array.rating} star review, and their feedback was: ${array.feedback}`)
+    return (`${array.name} gave the restaurant a ${array.rating} star review, and their feedback was: ${array.feedback}`)
 } 
 
 
